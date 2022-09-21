684ff25fbf70b589f994583ce9b2388da5890504
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2022-09-21 19:38:53 +0100

    Add Gradle build scans (#7)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
+plugins {
+    id("com.gradle.enterprise") version("3.11.1")
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "thecodinglove-kmp"
 
 apply {
     from("modules.gradle.kts")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
 }
