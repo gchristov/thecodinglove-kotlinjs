c131d1d67a2dd8e92a11aaa0848e4576766d566b
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2022-09-30 18:27:24 +0200

    Ensure correct dependency management and assembly for multi-module setup (#10)

--- appJs/appJs/build.gradle.kts
+++ appJs/appJs/build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
 }
 
 kotlin {
+    js(IR) {
+        compilations["main"].packageJson {
+            customField(
+                "dependencies", mapOf(
+                    "firebase" to "9.10.0",
+                    "firebase-admin" to "11.0.1",
+                    "firebase-functions" to "3.24.0",
+                )
+            )
+        }
+    }
     sourceSets {
         val main by getting {
             dependencies {
--- deploy/run_local_firebase.sh
+++ deploy/run_local_firebase.sh
@@ -1,4 +1,4 @@
-# Local development with Firebase
+# Builds and runs the project locally with the Firebase Emulator
 set -e
 sh ./deploy/build.sh functions
 firebase serve
