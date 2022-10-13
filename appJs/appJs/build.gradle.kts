4b4546d899f524e086ada9db47eb98f8e1a5c5d1
Georgi Christov <ga.christov@gmail.com>
2022-10-14 00:35:14 +0200

    Test 3

--- appJs/appJs/build.gradle.kts
+++ appJs/appJs/build.gradle.kts
@@ -61,6 +61,7 @@ buildkonfig {
 
 fun getApiKey(key: String): String {
     val propFile = rootProject.file("./local.properties")
+    println(propFile.exists())
     val properties = Properties()
     properties.load(FileInputStream(propFile))
     return properties.getProperty(key)
