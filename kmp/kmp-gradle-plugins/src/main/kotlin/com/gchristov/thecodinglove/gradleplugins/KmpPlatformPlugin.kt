4f4e8162c354899b36a4f914eaabe04685c926b1
Georgi Christov <ga.christov@gmail.com>
2022-11-15 11:54:32 +0100

    Cleanup core KMP gradle plugin

--- kmp/kmp-gradle-plugins/src/main/kotlin/com/gchristov/thecodinglove/gradleplugins/KmpPlatformPlugin.kt
+++ kmp/kmp-gradle-plugins/src/main/kotlin/com/gchristov/thecodinglove/gradleplugins/KmpPlatformPlugin.kt
@@ -3,8 +3,7 @@ package com.gchristov.thecodinglove.gradleplugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-@Suppress("unused")
-open class KmpPlatformPlugin : Plugin<Project> {
+abstract class KmpPlatformPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply("org.jetbrains.kotlin.multiplatform")
     }
