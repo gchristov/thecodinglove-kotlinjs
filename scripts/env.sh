00e2d9a077e3eafae6f18d5469e1b3454b4d41f4
Georgi Christov <ga.christov@gmail.com>
2022-12-19 14:45:41 +0100

    Add new env property

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,4 +6,5 @@ echo FIREBASE_PROJECT_ID="$FIREBASE_PROJECT_ID" >> ./local.properties
 echo FIREBASE_STORAGE_BUCKET="$FIREBASE_STORAGE_BUCKET" >> ./local.properties
 echo FIREBASE_GCM_SENDER_ID="$FIREBASE_GCM_SENDER_ID" >> ./local.properties
 echo FIREBASE_APPLICATION_ID="$FIREBASE_APPLICATION_ID" >> ./local.properties
+echo GCP_SA_KEY="$GCP_SA_KEY" >> ./local.properties
 echo SLACK_SIGNING_SECRET="$SLACK_SIGNING_SECRET" >> ./local.properties
