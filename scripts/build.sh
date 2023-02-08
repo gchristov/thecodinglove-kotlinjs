404f551e02c18ce0d6890212079e2d1346b4169f
Georgi Christov <ga.christov@gmail.com>
2023-02-08 10:18:51 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 # Builds the project and copies binaries to an output folder
 set -e
 ./gradlew assemble
-cp -R appBackend/appBackend/build/productionLibrary/. "build/productionBackend"
-cp -R appWeb/appWeb/build/distributions/. "build/productionWeb"
+cp -R backend/appBackend/build/productionLibrary/. "build/productionBackend"
+cp -R client/appWeb/appWeb/build/distributions/. "build/productionWeb"
 cd build/productionBackend && npm install && cd ..
