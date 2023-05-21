59de1f1d87d2796606094082ee85a65bcd538517
Georgi Christov <ga.christov@gmail.com>
2023-05-21 11:44:50 +0200

    Add more run script logs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 # Builds the project and copies binaries to an output folder
 set -e
-./gradlew assemble
-cp -R backend/appBackend/build/productionLibrary/. "build/productionBackend"
-cp -R client/appWeb/appWeb/build/distributions/. "build/productionWeb"
-cd build/productionBackend && npm install && cd ..
+echo "🛠 Assembling..." && ./gradlew assemble
+echo "📄 Copying backend files..." && cp -R backend/appBackend/build/productionLibrary/. "build/productionBackend"
+echo "📄 Copying frontend files..." && cp -R client/appWeb/appWeb/build/distributions/. "build/productionWeb"
+echo "🛠 Installing npm dependencies..." && cd build/productionBackend && npm install && cd ..
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 # Builds and runs the API project locally with the Firebase Emulator
 set -e
-echo "Building..." && sh ./scripts/build.sh
-echo "Starting local emulators..." && ssh -R 80:localhost:5000 serveo.net & ssh -R 80:localhost:5001 serveo.net & firebase emulators:start
+echo "🛠 Building..." && sh ./scripts/build.sh
+echo "🏁 Starting local emulators..." && ssh -R 80:localhost:5000 serveo.net & ssh -R 80:localhost:5001 serveo.net & firebase emulators:start
