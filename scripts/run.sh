34b26550cdd954a3440a815e05ea57a3930b4faa
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2023-05-21 10:52:34 +0100

    Update local run script to auto-launch the public ssh tunnels (#76)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 # Builds and runs the API project locally with the Firebase Emulator
 set -e
-sh ./scripts/build.sh
-firebase emulators:start
-firebase serve
+echo "🛠 Building..." && sh ./scripts/build.sh
+echo "🏁 Starting local emulators..." && ssh -R 80:localhost:5000 serveo.net & ssh -R 80:localhost:5001 serveo.net & firebase emulators:start
