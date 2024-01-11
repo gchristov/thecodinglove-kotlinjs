401f79fcb9fc40a73ddedb75ab0eee4b31896e76
Georgi Christov <ga.christov@gmail.com>
2024-01-11 17:15:01 +0100

    Attempt to setup the GCP infra SA key

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 # Exports required CI environment secrets to local environment files so that the project can use them
 set -e
 echo "$GCP_SA_KEY_APP" >> ./credentials-gcp-app.json
+echo "$GCP_SA_KEY_INFRA" >> /pulumi/credentials-gcp-infra.json
 echo SLACK_SIGNING_SECRET="$SLACK_SIGNING_SECRET" >> ./env.properties
 echo SLACK_REQUEST_VERIFICATION_ENABLED="$SLACK_REQUEST_VERIFICATION_ENABLED" >> ./env.properties
 echo SLACK_CLIENT_ID="$SLACK_CLIENT_ID" >> ./env.properties
