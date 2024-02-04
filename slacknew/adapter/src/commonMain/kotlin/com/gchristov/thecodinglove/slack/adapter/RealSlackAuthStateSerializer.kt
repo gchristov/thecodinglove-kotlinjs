a6d632d22bfd5521fcf799e1c3048bade64ade31
Georgi Christov <ga.christov@gmail.com>
2024-02-05 00:38:31 +0100

    Fix compile errors

--- slacknew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/slack/adapter/RealSlackAuthStateSerializer.kt
+++ slacknew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/slack/adapter/RealSlackAuthStateSerializer.kt
@@ -1,7 +1,7 @@
 package com.gchristov.thecodinglove.slack.adapter
 
 import com.gchristov.thecodinglove.common.kotlin.JsonSerializer
-import com.gchristov.thecodinglove.slack.adapter.http.model.ApiSlackAuthState
+import com.gchristov.thecodinglove.slack.adapter.http.mapper.toAuthState
 import com.gchristov.thecodinglove.slack.domain.model.SlackAuthState
 import com.gchristov.thecodinglove.slack.domain.ports.SlackAuthStateSerializer
 import io.ktor.util.*
@@ -9,6 +9,6 @@ import kotlinx.serialization.encodeToString
 
 class RealSlackAuthStateSerializer(private val jsonSerializer: JsonSerializer) : SlackAuthStateSerializer {
     override fun serialize(authState: SlackAuthState): String {
-        return jsonSerializer.json.encodeToString(ApiSlackAuthState.of(authState)).encodeBase64()
+        return jsonSerializer.json.encodeToString(authState.toAuthState()).encodeBase64()
     }
 }
--- slacknew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/slack/adapter/http/mapper/SlackAuthMapper.kt
+++ slacknew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/slack/adapter/http/mapper/SlackAuthMapper.kt
@@ -5,6 +5,15 @@ import com.gchristov.thecodinglove.slack.adapter.http.model.ApiSlackAuthState
 import com.gchristov.thecodinglove.slack.domain.model.SlackAuthState
 import com.gchristov.thecodinglove.slack.domain.model.SlackAuthToken
 
+internal fun SlackAuthState.toAuthState() = ApiSlackAuthState(
+    searchSessionId = searchSessionId,
+    channelId = channelId,
+    teamId = teamId,
+    userId = userId,
+    responseUrl = responseUrl,
+    selfDestructMinutes = selfDestructMinutes,
+)
+
 // TODO: Consider better differentiation between the token types
 internal fun ApiSlackAuthResponse.toAuthToken() = if (authedUser?.accessToken != null) {
     // User token
