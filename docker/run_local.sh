e395324e887ef0fc1e6f5916b2cbc1b7e629298b
Georgi Christov <ga.christov@gmail.com>
2024-02-08 21:07:50 +0100

    Update run command

--- docker/run_local.sh
+++ docker/run_local.sh
@@ -4,4 +4,4 @@ echo "🛠 Build project" && ./gradlew assemble
 echo "🧹 Clean up old Docker resources" && (docker image prune -af)
 echo "🏁 Start local tunnel" && (ssh -tt -R codinglove.serveo.net:80:localhost:8080 serveo.net &)
 sleep 1
-echo "🏁 Start app" && echo "" && docker compose -f docker/compose.yaml up --build
+echo "🏁 Start app" && echo "" && docker compose -f docker/compose.yaml up --build --remove-orphans
