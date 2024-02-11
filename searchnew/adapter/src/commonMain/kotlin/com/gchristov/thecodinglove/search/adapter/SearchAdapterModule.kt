d6f2f06d98c277a4f1760503c2488f6c4ae442fa
Georgi Christov <ga.christov@gmail.com>
2024-02-11 03:31:44 +0100

    Migrate search module

--- searchnew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/search/adapter/SearchAdapterModule.kt
+++ searchnew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/search/adapter/SearchAdapterModule.kt
@@ -1,14 +1,21 @@
 package com.gchristov.thecodinglove.search.adapter
 
+import co.touchlab.kermit.Logger
 import com.gchristov.thecodinglove.common.firebase.FirebaseAdmin
 import com.gchristov.thecodinglove.common.kotlin.JsonSerializer
 import com.gchristov.thecodinglove.common.kotlin.di.DiModule
 import com.gchristov.thecodinglove.common.network.NetworkClient
+import com.gchristov.thecodinglove.common.pubsub.PubSubDecoder
 import com.gchristov.thecodinglove.htmlparsedata.usecase.ParseHtmlPostsUseCase
 import com.gchristov.thecodinglove.htmlparsedata.usecase.ParseHtmlTotalPostsUseCase
+import com.gchristov.thecodinglove.search.adapter.http.PreloadSearchPubSubHandler
 import com.gchristov.thecodinglove.search.adapter.http.SearchApi
+import com.gchristov.thecodinglove.search.adapter.http.SearchHttpHandler
 import com.gchristov.thecodinglove.search.domain.model.SearchConfig
 import com.gchristov.thecodinglove.search.domain.port.SearchRepository
+import com.gchristov.thecodinglove.search.domain.usecase.PreloadSearchResultUseCase
+import com.gchristov.thecodinglove.search.domain.usecase.SearchUseCase
+import kotlinx.coroutines.Dispatchers
 import org.kodein.di.DI
 import org.kodein.di.bindProvider
 import org.kodein.di.bindSingleton
@@ -32,6 +39,21 @@ object SearchAdapterModule : DiModule() {
             bindSingleton { provideSearchConfig() }
             bindProvider { provideParseHtmlTotalPostsUseCase() }
             bindProvider { provideParseHtmlPostsUseCase() }
+            bindSingleton {
+                provideSearchHttpHandler(
+                    jsonSerializer = instance(),
+                    log = instance(),
+                    searchUseCase = instance(),
+                )
+            }
+            bindSingleton {
+                providePreloadSearchPubSubHttpHandler(
+                    jsonSerializer = instance(),
+                    log = instance(),
+                    preloadSearchResultUseCase = instance(),
+                    pubSubSubDecoder = instance(),
+                )
+            }
         }
     }
 
@@ -54,6 +76,30 @@ object SearchAdapterModule : DiModule() {
     private fun provideSearchConfig(): SearchConfig = SearchConfig(
         postsPerPage = 4,
         preloadPubSubTopic = BuildConfig.SEARCH_PRELOAD_PUBSUB_TOPIC,
+    )
+
+    private fun provideSearchHttpHandler(
+        jsonSerializer: JsonSerializer.Default,
+        log: Logger,
+        searchUseCase: SearchUseCase,
+    ): SearchHttpHandler = SearchHttpHandler(
+        dispatcher = Dispatchers.Default,
+        jsonSerializer = jsonSerializer,
+        log = log,
+        searchUseCase = searchUseCase,
+    )
+
+    private fun providePreloadSearchPubSubHttpHandler(
+        jsonSerializer: JsonSerializer.Default,
+        log: Logger,
+        preloadSearchResultUseCase: PreloadSearchResultUseCase,
+        pubSubSubDecoder: PubSubDecoder,
+    ): PreloadSearchPubSubHandler = PreloadSearchPubSubHandler(
+        dispatcher = Dispatchers.Default,
+        jsonSerializer = jsonSerializer,
+        log = log,
+        preloadSearchResultUseCase = preloadSearchResultUseCase,
+        pubSubDecoder = pubSubSubDecoder,
     )
 }
 
---
+++ searchnew/adapter/src/commonMain/kotlin/com/gchristov/thecodinglove/search/adapter/http/SearchHttpHandler.kt
@@ -0,0 +1,55 @@
+package com.gchristov.thecodinglove.search.adapter.http
+
+import arrow.core.Either
+import arrow.core.flatMap
+import co.touchlab.kermit.Logger
+import com.gchristov.thecodinglove.common.kotlin.JsonSerializer
+import com.gchristov.thecodinglove.common.network.http.*
+import com.gchristov.thecodinglove.search.adapter.http.mapper.toPost
+import com.gchristov.thecodinglove.search.adapter.http.model.ApiSearchResult
+import com.gchristov.thecodinglove.search.domain.usecase.SearchUseCase
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineDispatcher
+
+class SearchHttpHandler(
+    dispatcher: CoroutineDispatcher,
+    private val jsonSerializer: JsonSerializer,
+    log: Logger,
+    private val searchUseCase: SearchUseCase,
+) : BaseHttpHandler(
+    dispatcher = dispatcher,
+    jsonSerializer = jsonSerializer,
+    log = log
+) {
+    override fun httpConfig() = HttpHandler.HttpConfig(
+        method = HttpMethod.Get,
+        path = "/api/search",
+        contentType = ContentType.Application.Json,
+    )
+
+    override suspend fun handleHttpRequestAsync(
+        request: HttpRequest,
+        response: HttpResponse,
+    ): Either<Throwable, Unit> = searchUseCase(SearchUseCase.Dto(request.toSearchType()))
+        .flatMap { searchResult ->
+            response.sendJson(
+                data = searchResult.toSearchResult(),
+                jsonSerializer = jsonSerializer,
+            )
+        }
+}
+
+private fun HttpRequest.toSearchType(): SearchUseCase.Type {
+    val searchQuery: String = query["searchQuery"] ?: "release"
+    val searchSessionId: String? = query["searchSessionId"]
+    return searchSessionId?.let {
+        SearchUseCase.Type.WithSessionId(sessionId = it)
+    } ?: SearchUseCase.Type.NewSession(searchQuery)
+}
+
+private fun SearchUseCase.Result.toSearchResult() = ApiSearchResult(
+    searchSessionId = searchSessionId,
+    query = query,
+    post = post.toPost(),
+    totalPosts = totalPosts
+)
