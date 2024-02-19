9abf96833d3767e540fab0f454dd3a96d8d4f92d
Georgi Christov <ga.christov@gmail.com>
2024-02-19 18:35:31 +0100

    Working setup for web-proxy

--- landing-page/service/src/jsMain/kotlin/com/gchristov/thecodinglove/landingpage/service/LandingPageService.kt
+++ landing-page/service/src/jsMain/kotlin/com/gchristov/thecodinglove/landingpage/service/LandingPageService.kt
@@ -1,6 +1,6 @@
 package com.gchristov.thecodinglove.landingpage.service
 
 /*
- * The website still needs a source file in order to compile correctly. We use a placeholder since
- * There isn't any functionality needed yet.
+ * This web service needs a source file in order to compile correctly. We use a placeholder since there isn't any
+ * specific functionality needed yet.
  */
---
+++ web-proxy/service/src/jsMain/kotlin/com/gchristov/thecodinglove/webproxy/service/WebProxyService.kt
@@ -0,0 +1,6 @@
+package com.gchristov.thecodinglove.webproxy.service
+
+/*
+ * The web proxy needs a source file in order to compile correctly. We use a placeholder since there isn't any specific
+ * functionality needed yet.
+ */
