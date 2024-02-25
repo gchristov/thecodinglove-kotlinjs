e337fcf5cd070386e7992433a4e1259a1c5d6070
Georgi Christov <ga.christov@gmail.com>
2024-02-25 18:46:26 +0100

    Add self-destruct moduke

--- self-destruct/domain/build.gradle.kts
+++ self-destruct/domain/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     id("node-module-plugin")
 }
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.gchristov.thecodinglove.common:kotlin")
+            }
+        }
+    }
+}
