29a4f0c07dba7043f1b7b538598197738b58a86e
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2024-03-04 11:52:52 +0100

    Infra and naming polish (#152)

--- docker/run_local.sh
+++ docker/run_local.sh
@@ -5,11 +5,11 @@ echo "🧹 Clean up old Docker resources" && (docker image prune -af)
 echo "🏁 Start local tunnel" && (ssh -tt -R codinglove.serveo.net:80:localhost:8080 serveo.net &)
 sleep 1
 echo "🏁 Start app" && echo "" && docker compose \
--f docker/landing-page-web-service-compose.yaml \
--f docker/proxy-web-service-compose.yaml \
--f docker/search-service-compose.yaml \
--f docker/self-destruct-service-compose.yaml \
--f docker/slack-service-compose.yaml \
--f docker/slack-web-service-compose.yaml \
--f docker/statistics-service-compose.yaml \
+-f docker/landing-page-web-compose.yaml \
+-f docker/proxy-web-compose.yaml \
+-f docker/search-compose.yaml \
+-f docker/self-destruct-compose.yaml \
+-f docker/slack-compose.yaml \
+-f docker/slack-web-compose.yaml \
+-f docker/statistics-compose.yaml \
 up --build --remove-orphans
