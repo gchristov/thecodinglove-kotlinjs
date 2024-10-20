dcbf71192ab933e65509d7b36f4c69e0fa814758
Georgi Christov <ga.christov@gmail.com>
2024-10-20 22:00:42 +0200

    Move docker run scripts to tools directory

--- tools/scripts/run_local.sh
+++ tools/scripts/run_local.sh
@@ -5,11 +5,11 @@ echo "🧹 Clean up old Docker resources" && (docker image prune -af)
 echo "🏁 Start local tunnel" && (ssh -tt -R codinglove.serveo.net:80:localhost:8080 serveo.net &)
 sleep 1
 echo "🏁 Start app" && echo "" && docker compose \
--f docker/landing-page-web-compose.yaml \
--f docker/proxy-web-compose.yaml \
--f docker/search-compose.yaml \
--f docker/self-destruct-compose.yaml \
--f docker/slack-compose.yaml \
--f docker/slack-web-compose.yaml \
--f docker/statistics-compose.yaml \
+-f tools/docker/landing-page-web-compose.yaml \
+-f tools/docker/proxy-web-compose.yaml \
+-f tools/docker/search-compose.yaml \
+-f tools/docker/self-destruct-compose.yaml \
+-f tools/docker/slack-compose.yaml \
+-f tools/docker/slack-web-compose.yaml \
+-f tools/docker/statistics-compose.yaml \
 up --build --remove-orphans
