17883ba6921be14621455401178288ef9960220a
Georgi Christov <ga.christov@gmail.com>
2024-10-21 10:20:05 +0200

    Relative paths

--- tools/scripts/secrets.sh
+++ tools/scripts/secrets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-# Exports required CI environment secrets to local secrets so that the project can use them
+# Exports required CI environment secrets to local secrets so that the project can use them.
+# Should be invoked from the root of the project as all paths are relative.
 
 # Shared service GCP credentials
 echo "$GCP_SA_KEY_APP" >> ./credentials-gcp-app.json
@@ -12,14 +13,14 @@ for service in "${services[@]}"; do
 done
 
 ## common library credentials
-echo MONITORING_SLACK_URL="$MONITORING_SLACK_URL" >> common/monitoring/secrets.properties
-echo GOOGLE_ANALYTICS_MEASUREMENT_ID="$GOOGLE_ANALYTICS_MEASUREMENT_ID" >> common/analytics/secrets.properties
-echo GOOGLE_ANALYTICS_API_SECRET="$GOOGLE_ANALYTICS_API_SECRET" >> common/analytics/secrets.properties
+echo MONITORING_SLACK_URL="$MONITORING_SLACK_URL" >> ./common/monitoring/secrets.properties
+echo GOOGLE_ANALYTICS_MEASUREMENT_ID="$GOOGLE_ANALYTICS_MEASUREMENT_ID" >> ./common/analytics/secrets.properties
+echo GOOGLE_ANALYTICS_API_SECRET="$GOOGLE_ANALYTICS_API_SECRET" >> ./common/analytics/secrets.properties
 
 # slack service credentials
-echo SLACK_SIGNING_SECRET="$SLACK_SIGNING_SECRET" >> slack/domain/secrets.properties
-echo SLACK_CLIENT_ID="$SLACK_CLIENT_ID" >> slack/domain/secrets.properties
-echo SLACK_CLIENT_SECRET="$SLACK_CLIENT_SECRET" >> slack/domain/secrets.properties
+echo SLACK_SIGNING_SECRET="$SLACK_SIGNING_SECRET" >> ./slack/domain/secrets.properties
+echo SLACK_CLIENT_ID="$SLACK_CLIENT_ID" >> ./slack/domain/secrets.properties
+echo SLACK_CLIENT_SECRET="$SLACK_CLIENT_SECRET" >> ./slack/domain/secrets.properties
 
 # slack-web service credentials
-echo SLACK_CLIENT_ID="$SLACK_CLIENT_ID" >> slack-web/domain/secrets.properties
+echo SLACK_CLIENT_ID="$SLACK_CLIENT_ID" >> ./slack-web/domain/secrets.properties
