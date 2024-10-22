1af949b947bf493c0c2303a0a2ecaddd578e78f8
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2024-10-23 00:23:31 +0200

    Publish build scans from root project (#164)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    // Allows publishing build scans when common tasks run from the project root, eg jsTest
+    id("com.gradle.develocity") version("3.18.1")
+}
+
 rootProject.name = "thecodinglove-kotlinjs"
 
 // Add or remove projects here from the common build. Alternatively, each project can be opened in isolation.
@@ -10,3 +15,11 @@ includeBuild("self-destruct")
 includeBuild("slack")
 includeBuild("slack-web")
 includeBuild("statistics")
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+        publishing.onlyIf { true }
+    }
+}
