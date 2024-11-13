53aeba4f60f7786904d6c9d883aaaad23c611280
Georgi Christov <7644787+gchristov@users.noreply.github.com>
2024-11-13 23:19:27 +0000

    Nightly CI checks scaffold (#165)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 /*
 This file is intentionally empty as each sub-project should be independent. These tasks below allow us to execute
 common actions for all projects right from the project root.
-- clean, assemble and kotlinUpgradeYarnLock are used for local development
-- jsTest is used to run all unit tests in one go (eg within the CI)
+- clean and kotlinUpgradeYarnLock are used for local development
+- jsTest and assemble are used to run common tasks in one go for all services (eg within the CI)
 */
 val taskNames = listOf("clean", "assemble", "jsTest", "kotlinUpgradeYarnLock")
 taskNames.forEach { taskName ->
